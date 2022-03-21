e25c3a7669ac841ff107fda28b698eb699b64776
fireliu <fireliu@tencent.com>
2022-03-21 15:42:33 +0800

    feat（config）: 引入viper框架用于app配置

--- main.go
+++ main.go
@@ -2,16 +2,35 @@ package main
 
 import (
 	"errors"
+	"github.com/spf13/viper"
 	"log"
 	"net/http"
 	"time"
 
+	"apiserver/config"
 	"apiserver/router"
 
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/pflag"
+)
+
+var (
+	cfg = pflag.StringP("config", "c", "", "apiserver config file")
 )
 
 func main() {
+
+	pflag.Parse()
+
+	//init config
+	//在 main 函数中增加了 config.Init(*cfg) 调用，用来初始化配置，cfg 变量值从命令行 flag 传入，可以传值，比如 ./apiserver -c config.yaml，也可以为空，如果为空会默认读取 conf/config.yaml
+	if err := config.Init(*cfg); err != nil {
+		panic(err)
+	}
+
+	// Set gin mode.//gin 有 3 种运行模式：debug、release 和 test，其中 debug 模式会打印很多 debug 信息。
+	gin.SetMode(viper.GetString("runmode"))
+
 	//Create the Gin engine
 	g := gin.New()
 
@@ -27,8 +46,8 @@ func main() {
 		middlewares...,
 	)
 
-	log.Printf("Start to listening the incoming requests on http address: %s", "8080")
-	log.Printf(http.ListenAndServe(":8080", g).Error())
+	log.Printf("Start to listening the incoming requests on http address: %s", viper.GetString("addr"))
+	log.Printf(http.ListenAndServe(viper.GetString("addr"), g).Error())
 
 	go func() {
 		if err := pingServer(); err != nil {
@@ -41,9 +60,9 @@ func main() {
 
 func pingServer() error {
 
-	for i := 0; i < 10; i++ {
+	for i := 0; i < viper.GetInt("max_ping_count"); i++ {
 		// Ping the server by sending a GET request to `/health`.
-		resp, err := http.Get("http://127.0.0.1:8080" + "/sd/health")
+		resp, err := http.Get(viper.GetString("url") + "/sd/health")
 		if err == nil && resp.StatusCode == 200 {
 			return nil
 		}
